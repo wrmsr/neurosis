8dcd0e1a2b3ee517a54bd9b15debd21049e7aa6b
wrmsr <timwilloney@gmail.com>
2015-09-23 16:53:46 -0700

    --

--- neurosis-native/src/test/java/com/wrmsr/nativity/AppTest.java
+++ neurosis-native/src/test/java/com/wrmsr/nativity/AppTest.java
@@ -13,26 +13,6 @@
  */
 package com.wrmsr.nativity;
 
-import junit.framework.Assert;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 public class AppTest
-        extends TestCase
 {
-    public AppTest(String testName)
-    {
-        super(testName);
-    }
-
-    public static Test suite()
-    {
-        return new TestSuite(AppTest.class);
-    }
-
-    public void testApp()
-    {
-        Assert.assertTrue(true);
-    }
 }
